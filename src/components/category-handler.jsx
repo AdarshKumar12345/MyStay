a9913b4cd6bc172000f5727e86bbb82dfe8eb6a0
Adarsh Kumar <adarsh8507kumar@gmail.com>
2025-06-30 20:47:12 +0530

    4 th commit

--- src/components/category-handler.jsx
+++ src/components/category-handler.jsx
@@ -1,36 +1,45 @@
-"use client"
+'use client';
 
-import { cn } from "@/lib/utils";
-import { categories } from "@/static/config"
-import { useRouter, useSearchParams } from "next/navigation"
+import React, { Suspense } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { categories } from '@/static/config';
 
-export default function CategoryHandler(){
-    const searchParms = useSearchParams();
-    const router = useRouter();
+function CategoryComponent() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-    const activeCat = searchParms.get('cat');
+  const activeCat = searchParams.get('cat');
+  const params = new URLSearchParams(searchParams.toString());
 
-    const params = new URLSearchParams(searchParms.toString());
+  const setCategory = (cat) => {
+    params.set('cat', cat);
+    router.push(`?${params.toString()}`);
+  };
 
-    const setCategory = (cat) => {
-        params.set('cat', cat)
-        router.push(`?${params.toString()}`)
-    }
-
-
-    return (
-        <div className="w-full flex justify-evenly gap-3 py-2 px-8 border-b border-gray-100 overflow-x-auto">
-
-            {categories.map(cat => {
-                return <div onClick={()=> setCategory(cat.label)} key={cat.label} 
-                        className={cn(
-                            "flex flex-col gap-1 items-center cursor-pointer hover:bg-gray-100/40 p-4 rounded-lg hover:text-red-500 transition-colors duration-200 delay-100",
-                            activeCat == cat.label && "bg-gray-100/40 text-red-400"
-                            )}>
-                        <cat.icon />
-                        {cat.label}
-                    </div>
-            })}
+  return (
+    <div className="w-full flex justify-evenly gap-3 py-2 px-8 border-b border-gray-100 overflow-x-auto">
+      {categories.map((cat) => (
+        <div
+          onClick={() => setCategory(cat.label)}
+          key={cat.label}
+          className={cn(
+            'flex flex-col gap-1 items-center cursor-pointer hover:bg-gray-100/40 p-4 rounded-lg hover:text-red-500 transition-colors duration-200 delay-100',
+            activeCat === cat.label && 'bg-gray-100/40 text-red-400'
+          )}
+        >
+          <cat.icon />
+          {cat.label}
         </div>
-    )
+      ))}
+    </div>
+  );
+}
+
+export default function CategoryHandler() {
+  return (
+    <Suspense fallback={null}>
+      <CategoryComponent />
+    </Suspense>
+  );
 }
