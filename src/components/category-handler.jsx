e72fa50fe8e642644c3f05b5ee34ba11f6fed986
Adarsh Kumar <adarsh8507kumar@gmail.com>
2025-06-30 21:23:10 +0530

    6th commit

--- src/components/category-handler.jsx
+++ src/components/category-handler.jsx
@@ -1,45 +1,10 @@
-'use client';
-
-import React, { Suspense } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { cn } from '@/lib/utils';
-import { categories } from '@/static/config';
-
-function CategoryComponent() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const activeCat = searchParams.get('cat');
-  const params = new URLSearchParams(searchParams.toString());
-
-  const setCategory = (cat) => {
-    params.set('cat', cat);
-    router.push(`?${params.toString()}`);
-  };
-
-  return (
-    <div className="w-full flex justify-evenly gap-3 py-2 px-8 border-b border-gray-100 overflow-x-auto">
-      {categories.map((cat) => (
-        <div
-          onClick={() => setCategory(cat.label)}
-          key={cat.label}
-          className={cn(
-            'flex flex-col gap-1 items-center cursor-pointer hover:bg-gray-100/40 p-4 rounded-lg hover:text-red-500 transition-colors duration-200 delay-100',
-            activeCat === cat.label && 'bg-gray-100/40 text-red-400'
-          )}
-        >
-          <cat.icon />
-          {cat.label}
-        </div>
-      ))}
-    </div>
-  );
-}
+import { Suspense } from "react";
+import CategoryHandlerContent from "./CategoryHandlerContent";
 
 export default function CategoryHandler() {
   return (
-    <Suspense fallback={null}>
-      <CategoryComponent />
+    <Suspense fallback={<div>Loading categories...</div>}>
+      <CategoryHandlerContent />
     </Suspense>
   );
 }
